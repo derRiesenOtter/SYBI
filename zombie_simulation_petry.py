875129b4e66e44f4f4cdbc8cc2f940543a2f451e
derRiesenOtter <robin.ender@icloud.com>
2024-08-24 16:10:05 +0200

    petry net added

--- zombie_simulation_petry.py
+++ zombie_simulation_petry.py
@@ -3,34 +3,39 @@ import random
 import matplotlib.pyplot as plt
 import numpy as np
 
-BASE_CASE = {
-    "infected": 0.000001,
-    "contact_rate": 10,
-    "infection_probability": 0.15,
+case = {
+    "case_name": "PETRY_NET",
+    "infection_probability": 0.8,
     "human_killed_rate": 0.15,
-    "zombie_killed_rate": 1,
-    "time_in_days": 360,
+    "zombie_killed_rate": 0.05,
 }
 
-POPULATION = {"susceptible": 999999, "infected": 1, "removed": 0}
+population = {"susceptible": 999999, "infected": 1, "removed": 0}
 
 
-def petry(population):
+def petry(population, case):
     if population["susceptible"] <= 0 & population["infected"] <= 0:
         return population
-    for sus in range(population["susceptible"]):
-        for contact in range(10):
-            if random.random() <= (population["infected"] / population["susceptible"]):
-                if random.random() < 0.15:
-                    population = infection(population)
-                    break
-    for inf in range(population["infected"]):
-        if random.random() <= 0.15:
-            population = human_killed(population)
-    for sus in range(population["susceptible"]):
-        if random.random() <= 0.15:
-            population = zombie_killed(population)
-    return population
+    zombie_encounter_chance = (
+        population["infected"]
+        * population["susceptible"]
+        / (
+            (population["infected"] + population["susceptible"])
+            * (population["infected"] + population["susceptible"])
+            / 4
+        )
+    )
+    infection_chance = zombie_encounter_chance * case["infection_probability"]
+    human_killed_chance = zombie_encounter_chance * case["human_killed_rate"]
+    zombie_killed_chance = zombie_encounter_chance * case["zombie_killed_rate"]
+    all_chances = infection_chance + human_killed_chance + zombie_killed_chance
+    decider = random.random()
+    if decider < infection_chance:
+        return infection(population)
+    elif decider < (infection_chance + human_killed_chance):
+        return human_killed(population)
+    elif decider < all_chances:
+        return zombie_killed(population)
 
 
 def infection(population):
@@ -51,4 +56,31 @@ def zombie_killed(population):
     return population
 
 
-print(petry(POPULATION))
+def main():
+    results = [[], [], []]
+    moves = 10000000
+    step_size = int((population["susceptible"] + population["infected"]) / 1000)
+    counter = 0
+    for i in range(moves):
+        petry(population, case)
+        counter += 1
+        if counter == step_size:
+            results[0].append(population["susceptible"])
+            results[1].append(population["infected"])
+            results[2].append(population["removed"])
+            step_size = int((population["susceptible"] + population["infected"]) / 1000)
+            counter = 0
+    plt.plot(np.arange(len(results[0])), results[0], label="Susceptible", color="g")
+    plt.plot(np.arange(len(results[0])), results[1], label="Infected", color="r")
+    plt.plot(np.arange(len(results[0])), results[2], label="Removed", color="black")
+    plt.xlabel("Time in hours")
+    plt.ylabel("People")
+    plt.title("Petry Net Simulation")
+    plt.ylim(0, 1000000)
+    plt.legend()
+    plt.savefig("figures/" + case["case_name"] + ".png")
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
